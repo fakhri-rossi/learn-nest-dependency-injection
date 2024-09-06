08717332c62d1f027f2762a45787db9606b0677d
Fakhri Rossi <rossifakhri@gmail.com>
2024-09-06 19:54:48 +0700

    Disk service: Power Service Injection

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,11 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('CPU needs 10 watts to perform this operation');
+    this.powerService.supplyPower(10, 'CPU');
+
+    return a + b;
+  }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log('Disk needs 20 watts to return a data');
+    this.powerService.supplyPower(20, 'Disk');
+    return 'Data from disk';
+  }
+}
